dde6906af904985a549d2af58aa7c3ef023c7418
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-04-28 15:30:01 +0300

    q

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,26 @@ import transactions from 'components/TransactionHistory/transactions.json';
 export default function App() {
   return (
     <div className="tasks">
-      <div>
-        <Profile
-          name={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          followers={user.stats.followers}
-          views={user.stats.views}
-          likes={user.stats.likes}
-        />
-      </div>
-      <div>
-        <Statistics title="Upload stats" stats={data} />
-      </div>
-      <div>
-        <FriendList friends={friends} />
-      </div>
-      <div>
-        <TransactionHistory items={transactions} />
-      </div>
+      {/* <div> */}
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      {/* </div> */}
+      {/* <div> */}
+      <Statistics title="Upload stats" stats={data} />
+      {/* </div> */}
+      {/* <div> */}
+      <FriendList friends={friends} />
+      {/* </div> */}
+      {/* <div> */}
+      <TransactionHistory items={transactions} />
+      {/* </div> */}
     </div>
   );
 }
